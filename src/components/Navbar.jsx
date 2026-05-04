0594455ddd38f3673041b888a37fcfeb8eb7063b
Njabulo Bhendane <njabulobhendane1996@gmail.com>
2026-05-04 16:13:03 +0200

    reverted everything back

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,14 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
 
 function Navbar() {
-  const [menuOpen, setMenuOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-  };
-
-  const closeMenu = () => {
-    setMenuOpen(false);
-  };
-
   return (
     <nav className="navbar">
-      <h1>Bhendane Family</h1>
-
-      {/* Burger Icon */}
-      <div className="burger" onClick={toggleMenu}>
-        ☰
-      </div>
-
-      {/* Navigation Links */}
-      <div className={`nav-links ${menuOpen ? "active" : ""}`}>
-        <Link to="/" onClick={closeMenu}>Home</Link>
-        <Link to="/13thsage" onClick={closeMenu}>13th Sage</Link>
-        <Link to="/bakery" onClick={closeMenu}>B's Bakery</Link>
-        <Link to="/comfy" onClick={closeMenu}>Comfy Food</Link>
+      <h1>BHENDANE</h1>
+      <div className="nav-links">
+        <Link to="/">Home</Link>
+        <Link to="/13thsage">13th Sage</Link>
+        <Link to="/bakery">Bee's Bakery</Link>
+        <Link to="/comfy">Comfy Food</Link>
       </div>
     </nav>
   );
